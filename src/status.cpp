99b737edced3a7eb43fed3f4102c943b2dda9162
mykeduong <hongminh4402@gmail.com>
2024-11-23 20:32:35 -0800

    refactoring

--- src/status.cpp
+++ src/status.cpp
@@ -9,16 +9,17 @@ void VerdantStatus::handleError(VerdantStatus::StatusEnum status) {
   std::string statusString;
 
   switch (status) {
-    case VerdantStatus::INTERNAL_ERROR: {
-      statusString = "INTERNAL_ERROR";
-      break;
-    }
-    case VerdantStatus::SUCCESS: {
-      statusString = "SUCCESS";
-      break;
-    }
+  case VerdantStatus::INTERNAL_ERROR: {
+    statusString = "INTERNAL_ERROR";
+    break;
+  }
+  case VerdantStatus::SUCCESS: {
+    statusString = "SUCCESS";
+    break;
+  }
   }
 
-  std::cerr << "[ERROR] An unrecoverable error has occurred with error code " << statusString << "." << std::endl;
+  std::cerr << "[ERROR] An unrecoverable error has occurred with error code "
+            << statusString << "." << std::endl;
   std::exit(status);
 }
